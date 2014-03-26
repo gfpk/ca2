1f49df0a3539c115c04fdf011483f2d36c26aa8a
Grzegorz Filipek <kongu3333@gmail.com>
2014-03-26 19:53:32 +0000

    tests done

--- js/calculator.js
+++ js/calculator.js
@@ -9,7 +9,7 @@ var Calculator = (function() {
         addIt: function(num1, num2) {
 
             if ((isNaN(num1) == true) || (isNaN(num2) == true)  ) {
-                throw new Error ("One of fields is not an number");
+                throw new Error ("at least of fields is not an number");
             }
             result = (parseInt(num1)+ parseInt(num2));
 
@@ -19,7 +19,7 @@ var Calculator = (function() {
         subtractIt: function(num1, num2) {
 
             if ((isNaN(num1) == true) || (isNaN(num2) == true)  ) {
-                throw new Error ("One of fields is not an number");
+                throw new Error ("at least of fields is not an number");
             }
             result = (parseInt(num1)- parseInt(num2));
 
@@ -29,7 +29,7 @@ var Calculator = (function() {
         multiplyIt: function(num1, num2) {
 
             if ((isNaN(num1) == true) || (isNaN(num2) == true)  ) {
-                throw new Error ("One of fields is not an number");
+                throw new Error ("at least of fields is not an number");
             }
             result = (parseInt(num1)* parseInt(num2));
 
@@ -39,7 +39,10 @@ var Calculator = (function() {
         divideIt: function(num1, num2) {
 
             if ((isNaN(num1) == true) || (isNaN(num2) == true)  ) {
-                throw new Error ("One of fields is not an number");
+                throw new Error ("at least of fields is not an number");
+            }
+            else if (num2 == 0  ) {
+                throw new Error ("Cannot divide by '0' Hombre");
             }
             result = (parseInt(num1)/ parseInt(num2));
 
--- test/spec/calculatorSpec.js
+++ test/spec/calculatorSpec.js
@@ -1,8 +1,20 @@
-describe("My Converter", function() {
-    it("should convert imperial weight to metric", function() {
-        expect(Converter.convertFromPoundsToKIlo(2.2)).toBe(1);
+describe("Calculator", function() {
+    it("should be able to add numbers", function() {
+        expect(Calculator.addIt(2,3)).toBe(5);
+    })
+    it("should be able to subtract numbers", function() {
+        expect(Calculator.subtractIt(2,3)).toBe(-1);
+    })
+    it("should be able to multiply numbers", function() {
+        expect(Calculator.multiplyIt(2,3)).toBe(6);
+    })
+    it("should be able to divide numbers", function() {
+        expect(Calculator.divideIt(9,3)).toBe(3);
+    })
+    it("should be able avoid zero division problem", function(){
+        expect(function() {Calculator.divideIt(12, 0)}).toThrow(new Error("Cannot divide by '0' Hombre"))
     })
     it("should be able to deal with strings", function(){
-        expect(function() {Converter.convertFromPoundsToKIlo("hello")}).toThrow(new Error("Not a number"))
+        expect(function() {Calculator.addIt("sausage", 4)}).toThrow(new Error("at least of fields is not an number"))
     })
 })
