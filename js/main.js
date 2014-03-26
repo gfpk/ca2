d22985ad78e097aca1b3442712abe95f77675b13
Grzegorz Filipek <kongu3333@gmail.com>
2014-03-26 18:23:10 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+window._skel_config = {
+    prefix: 'css/skel/style',
+    preloadStyleSheets: true,
+    resetCSS: true,
+    boxModel: 'border',
+    grid: { gutters: 20 },
+    breakpoints: {
+        wide: { range: '1200-', containers: 1140, grid: { gutters: 10 } },
+        narrow: { range: '481-1199', containers: 960 },
+        mobile: { range: '-480', containers: 'fluid', lockViewport: true, grid: { collapse: true } }
+
+    },
+};
+
+$(document).ready(function() {
+    $("#convertbutton").click(function() {
+        var metricValue = $("#metricinput").val();
+        var imperialValue = $("#imperialinput").val();
+
+
+        if (((metricValue.length !=0) && (imperialValue.length !=0))
+            || ((metricValue.length == 0) && imperialValue.length == 0)) {
+            alert("Please set one field to convert")
+        }
+        if (metricValue.length != 0) {
+            $("#imperialinput").val(Converter.convertFromPoundsToKIlo(metricValue));
+        }
+        else if (imperialValue.length != 0) {
+            $("#metricinput").val(Converter.convertFromMetricToImperial(imperialValue));
+        }
+    });
+    $("#clearbutton").click(function() {
+        $("#imperialinput").val('');
+        $("#metricinput").val('');
+    });
+
+});
